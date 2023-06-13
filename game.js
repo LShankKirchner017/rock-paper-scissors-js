99f3d338e5d37cb773fa213dd225d5541fcc52e6
Lindsay <lshank017@gmail.com>
2023-06-12 21:09:47 -0500

    added choices and outcome alerts

--- game.js
+++ game.js
@@ -6,26 +6,36 @@ var ties = 0;
 var choices = ["R", "P", "S"]
 
 
-    // human choice - get choice from user input
-// var humanChoice = prompt("Choose R, P, or S")
-// console.log(humanChoice)
+// human choice
+var humanChoice = prompt("Choose R, P, or S")
 
-// computer randomly chooses
+
+// random computer choice
 var randomNum = Math.floor( Math.random() * 3 )
-console.log(choices [randomNum])
+var computerChoice = (choices [randomNum])
 
+console.log("Human: " + humanChoice)
+console.log("Computer: " + computerChoice)
 //compare choices
-    // if humanChoice is S and computerChoice is P OR
-    // if humanChoice is R and computerChoice is S OR
-    // if humanCHoice is P and computerChoice is R
-        // alert that we won
-        // increase wins
-    // if humanChoice === computerChoice
-        // alert that we tied
-        //increase ties
-//else
-    // alert that we lost
+if(
+   humanChoice === "S" && computerChoice === "P" ||
+    humanChoice === "R" && computerChoice === "S" ||
+    humanChoice === "P" && computerChoice === "R" 
+
+)  {
+    alert("You won!")
+
+} else if (humanChoice === computerChoice) {
+ alert("Round tied!")
+} else {
+     alert("You lost!")
     //increase losses
+
+}
+
+
+    
+        
 
 // ask user "do you want to play again?"
     //if yes
