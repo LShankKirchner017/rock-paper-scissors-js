c1c99c590c52eeb246b9bc6399a84a562b17d21a
Lindsay <lshank017@gmail.com>
2023-06-12 20:34:58 -0500

    pseudo code

--- game.js
+++ game.js
@@ -1,1 +1,8 @@
-console.log("hello rps!");
+// players choose R, P S
+// human choice - get choice from user input
+// computer randomly chooses R, P, S
+
+//compser choices
+// wins
+// ties
+// loses
